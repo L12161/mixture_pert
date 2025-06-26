f2b1ec3e652766922446d436555ec1fe89907fda
maisoon <mdmaisoonrahman@ku.edu>
2025-06-26 03:50:07 -0500

    big changes
    added the loops_over_privacy_parameters and privacy_parameters.m
    
    They read the table of epsilon values stored as csv in downloads folder. Also the number of unique_classes from download folder. The tables were generated for PubMed dataset. In the end, after running the loop_over.. , we get a csv file. That contains a_values, b_values, p_values, alpha_values, etc. These values are then used to perturbe the pubmed dataset, which is then used for GNN training.

---
+++ loops_over_privacy_parameters.m
@@ -0,0 +1,43 @@
+clear all;
+close all;
+clc;
+
+data = readtable("C:\Users\maiso\Downloads\pubmed_probabilistic_data.csv");
+unique_counts = readtable("C:\Users\maiso\Downloads\pubmed_unique_counts.csv");
+[unique_epsilon_vals, props] = reading_distribution(data);
+
+all_rows = [];
+
+for i = 2:length(props) % loop was started from 0 as the first column contained the user number, which is irrelevent. If the user number was not converted in the reading 
+    % distribution function for a custom dataset, this value might need to
+    % be modified. 
+    [a_values, b_values, alpha_values, p_values, q_values] = privacy_parameters(unique_epsilon_vals{i}', props{i}', unique_counts{i-1,:}(2));
+    
+    %disp(alpha_values)
+    % Ensure row vectors
+    a_values = a_values(:)';
+    b_values = b_values(:)';
+    p_values = p_values(:)';
+    q_values = q_values(:)';
+    row = [a_values, b_values, alpha_values, p_values, q_values];
+
+    % Append to master matrix
+    all_rows = [all_rows; row];
+end
+
+% Define headers
+headers = {'a1', 'a2', 'a3', 'b1', 'b2', 'b3', ...
+           'alpha1',...
+           'p1', 'p2', 'p3', 'q1', 'q2', 'q3'};
+
+% Write to CSV
+csv_filename = 'C:\Users\maiso\Downloads\privacy_parameters_output.csv';
+% I'm keping the outputs to download folder 
+fid = fopen(csv_filename, 'w');
+fprintf(fid, '%s,', headers{1:end-1});
+fprintf(fid, '%s\n', headers{end});
+fclose(fid);
+
+% Append data
+dlmwrite(csv_filename, all_rows, '-append');
+disp("DONE CONVERTING TO CSV")
---
+++ privacy_parameters.m
@@ -0,0 +1,68 @@
+function [a_values, b_values, alpha_values, p_values, q_values] = privacy_parameters(W, Prob, d)
+% ANALYZE_PRIVACY_PARAMETERS Calculate privacy mechanism parameters
+%   [a, b, alpha, p, q] = analyze_privacy_parameters(W, Prob, N_user)
+%
+% Inputs:
+%   W - Array of privacy weight levels (e.g., [1 1.2 2])
+%   Prob - Probability distribution for each level (e.g., [0.05 0.05 0.9])
+%   N_user - Number of users in the dataset
+%
+% Outputs:
+%   a_values - Optimal a parameters for each epsilon level
+%   b_values - Optimal b parameters for each epsilon level
+%   alpha_values - Alpha parameters for mixture mechanism
+%   p_values - p parameters for GRR component
+%   q_values - q parameters for GRR component
+
+% Initialize parameters
+N_lev = length(W);
+N_loc = 100;  % Default location parameter
+alpha = N_loc * Prob;
+
+% Load or generate data (placeholder - modify as needed)
+% data = generate_exponential(N_user, N_loc);
+
+% Define optimization functions
+fun0 = @(x) alpha * ((x(N_lev+1:end) - x(N_lev+1:end).^2) ./ ((x(1:N_lev)-x(N_lev+1:end)).^2)) ...
+    + max((1-x(1:N_lev)-x(N_lev+1:end)) ./ (x(1:N_lev)-x(N_lev+1:end)));
+
+fun1 = @(x) alpha * (exp(x) ./ ((exp(x)-1).^2)); % symmetric
+fun2 = @(b) alpha * ((b-b.^2) ./ ((0.5-b).^2)) + 1; % a = 0.5
+
+% Epsilon values to evaluate
+myEpsilon = 1;
+N_epsilon = length(myEpsilon);
+
+% Initialize output variables
+a_values = [];
+b_values = [];
+alpha_values = [];
+p_values = [];
+q_values = [];
+
+% Main optimization loop
+for i = 1:N_epsilon
+    epsilon = myEpsilon(i);
+    temp = W * epsilon;
+    
+    % Optimization for mixture mechanism
+    fun3 = @(x)  x(4*N_lev+1)*sum((x(2*N_lev+1 : 3*N_lev).*(1-x(2*N_lev+1 : 3*N_lev)))./(x(2*N_lev+1 : 3*N_lev)-x(3*N_lev+1 : 4*N_lev)).^2) ...
+        +...
+        alpha*((( ((1 - x(4*N_lev+1:end)) ).*x(N_lev+1:2*N_lev) - ((1 - x(4*N_lev+1:end))).*x(N_lev+1:2*N_lev).^2 )) ...
+        ./ ...
+        ((x(1:N_lev)-x(N_lev+1:2*N_lev)).^2))...
+        + ...
+        max( ((1 - x(4*N_lev+1:end)) - (1 - x(4*N_lev+1:end)).*x(1:N_lev) - (1 - x(4*N_lev+1:end)).*x(N_lev+1:2*N_lev)) ...
+        ./ ...
+        (x(1:N_lev)-x(N_lev+1:2*N_lev)) );
+    
+    [X, ~] = min_opt3(temp, fun3, d);
+    
+    % Store results
+    a_values = [a_values; X(1:N_lev)];
+    b_values = [b_values; X(N_lev+1:2*N_lev)];
+    p_values = [p_values; X(2*N_lev+1:3*N_lev)];
+    q_values = [q_values; X(3*N_lev+1:4*N_lev)];
+    alpha_values = [alpha_values; X(4*N_lev+1:end)];
+end
+end
